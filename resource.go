5f663850396c11b7d8964514c180c63066a92736
tudyzhb <tudyzhb@gmail.com>
2019-10-16 22:47:29 +0800

    feat: 主动拉取群消息

--- resource.go
+++ resource.go
@@ -1,9 +1,6 @@
 package wkteam
 
 import (
-	"github.com/suboat/go-contrib"
-
-	"encoding/json"
 	"time"
 )
 
@@ -13,15 +10,33 @@ import (
 type Query struct {
 	Account string                 // 要管理的微信号
 	Skip    int                    // 分页参数
-	Page    int                    // 分页参数,填写会覆盖Skip信息
+	Page    int                    // 分页参数,填写会覆盖Skip信息 0起始
 	Limit   int                    // 分页参数,默认10
 	Params  map[string]interface{} // 其它参数
+	// 回应参数
+	Total int // 总数据量
 }
 
 // 群信息
 type Group struct {
 	Gid  string `json:"number"` // 群唯一ID
 	Name string `json:"name"`   // 群名
+	// 别名字段
+}
+
+// 群消息
+type MsgGroup struct {
+	Gid       string    `json:"gid"`            // 群唯一ID
+	Uid       string    `json:"wac_account"`    // 消息发送者唯一ID
+	Name      string    `json:"wac_name"`       // 消息发送者昵称
+	NameAlias string    `json:"form_name"`      // 消息发送者备注
+	Content   string    `json:"content"`        // 消息内容
+	Time      time.Time `json:"time,omitempty"` // 消息创建时间
+	//
+	ID         int   `json:"id,omitempty"`          //
+	GroId      int   `json:"gro_id,omitempty"`      //
+	CreateTime int64 `json:"create_time,omitempty"` //
+	inited     bool  //
 }
 
 // 开发者信息
@@ -35,8 +50,30 @@ type Agent struct {
 	//
 	OverdueTime int64 `json:"overdue_time,omitempty"` //
 	LastTime    int64 `json:"last_time,omitempty"`    //
-	//
-	inited bool //
+	inited      bool  //
+}
+
+// 整理为go友好数据格式
+func (d *MsgGroup) init() (err error) {
+	if d.inited {
+		return
+	}
+	d.Time = time.Unix(d.CreateTime, 0)
+	d.inited = true
+	return
+}
+
+//
+func (d *MsgGroup) GetName() string {
+	if d == nil {
+		return ""
+	} else if len(d.NameAlias) > 0 {
+		return d.NameAlias
+	} else if len(d.NameAlias) > 0 {
+		return d.Name
+	} else {
+		return d.Uid
+	}
 }
 
 // 整理为go友好数据格式
@@ -48,15 +85,4 @@ func (d *Agent) init() (err error) {
 	d.TimeLogin = time.Unix(d.LastTime, 0)
 	d.inited = true
 	return
-}
-
-// 解析返回
-func (d *Agent) Parse(resp string) (err error) {
-	if d == nil {
-		return contrib.ErrUndefined
-	} else if err = json.Unmarshal([]byte(resp), d); err != nil {
-		println(err.Error())
-		return
-	}
-	return d.init()
 }
